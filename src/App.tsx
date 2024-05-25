6ebea665241a8c2b63561edfe81e46482f16f34c
NhanDev <nhandev.bitas@gmail.com>
2024-05-25 16:08:03 +0700

    update config, delete ui lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,33 @@
-import { FC } from 'react';
-import reactlogo from './assets/images/react.svg';
-import './assets/styles/app.css';
-import { Accordion, AccordionItem, AccordionTrigger, AccordionContent, Badge } from './components';
+import React from 'react';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from 'beta-parity-react/ui/Accordion';
 
-const App: FC = () => {
+const App = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <img src={reactlogo} alt='react' width={100} height={100} />
+    <Accordion
+      style={{ width: '500px', margin: '50px' }}
+      type='single'
+      defaultValue=''
+      collapsible
+      className='Accordion'
+      onValueChange={(value) => console.log(value)}
+    >
+      <AccordionItem value='item-1'>
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+      </AccordionItem>
 
-      <h2>Demo for Accordion</h2>
+      <AccordionItem value='item-2'>
+        <AccordionTrigger>Is it unstyled?</AccordionTrigger>
+        <AccordionContent>Yes. Its unstyled by default, giving you freedom over the look and feel.</AccordionContent>
+      </AccordionItem>
 
-      <Accordion
-        style={{ width: '500px', margin: '50px' }}
-        type='single'
-        defaultValue='item-1'
-        className='Accordion'
-        onValueChange={(value) => console.log(value)}
-      >
-        <AccordionItem value='item-1'>
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
-        </AccordionItem>
-
-        <AccordionItem value='item-2'>
-          <AccordionTrigger>Is it unstyled?</AccordionTrigger>
-          <AccordionContent>Yes. Its unstyled by default, giving you freedom over the look and feel.</AccordionContent>
-        </AccordionItem>
-
-        <AccordionItem value='item-3'>
-          <AccordionTrigger>Can it be animated?</AccordionTrigger>
-          <AccordionContent>
-            <div>Yes! You can animate the Accordion with CSS or JavaScript.</div>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-
-      <h2>Demo for Badge</h2>
-
-      <Badge color='red'>Base</Badge>
-    </div>
+      <AccordionItem value='item-3'>
+        <AccordionTrigger>Can it be animated?</AccordionTrigger>
+        <AccordionContent>
+          <div>Yes! You can animate the Accordion with CSS or JavaScript.</div>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 };
 
--- ui/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './Accodion';
-export * from './Badge';
